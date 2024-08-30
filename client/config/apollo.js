aa9ac0890bbff22027433b6c66d68a4aac022762
mnaufaldillah <mnaufaldillah@gmail.com>
2024-08-30 17:39:41 +0700

    Update apollo.js

--- client/config/apollo.js
+++ client/config/apollo.js
@@ -3,7 +3,7 @@ import { setContext } from "@apollo/client/link/context";
 import * as SecureStore from "expo-secure-store";
 
 const httpLink = createHttpLink({
-    uri: `https://naufal.aryajati.my.id`
+    uri: `https://32cf-103-165-209-195.ngrok-free.app`
 });
 
 const authLink = setContext(async (_, { headers }) => {
